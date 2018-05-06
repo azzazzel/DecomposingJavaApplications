cd4110b5207b21de144efc09d56fe492f17833b6
Milen Dyankov <milendyankov@gmail.com>
2018-05-06 17:27:46 +0300

    Updated example 8

--- HouseCleaning8/HouseCleaning8-frontoffice/src/main/java/dja/housecleaning/frontoffice/FrontOffice.java
+++ HouseCleaning8/HouseCleaning8-frontoffice/src/main/java/dja/housecleaning/frontoffice/FrontOffice.java
@@ -1,12 +1,18 @@
 package dja.housecleaning.frontoffice;
 
+import java.util.List;
+
 import org.joda.money.Money;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
+import dja.housecleaning.company.internal.assets.Storage;
+import dja.housecleaning.company.internal.assets.Vehicles;
 import dja.housecleaning.frontoffice.internal.FrontOfficeCleaningRequest;
 import dja.housecleaning.usecases.CleaningRequest;
 import dja.housecleaning.usecases.CustomerUsecases;
+import other.things.CleaningTool;
+import other.things.Van;
 
 @Component (service=FrontOffice.class)
 public class FrontOffice {
@@ -24,12 +30,14 @@ public class FrontOffice {
 		customerUsecases.cleanCustomerHouse(cleaningRequest);
 	}
 
-//	public Van borrowVan(String who) {
-//		return Vehicles.companyVan;
-//	}
-//
-//	public List<CleaningTool> borrowTools(String who) {
-//		return Storage.PLACE.getCleaningTools();
-//	}
+	public Van borrowVan(String who) {
+		// return Vehicles.companyVan;
+		throw new UnsupportedOperationException();
+	}
+
+	public List<CleaningTool> borrowTools(String who) {
+		// return Storage.PLACE.getCleaningTools();
+		throw new UnsupportedOperationException();
+	}
 
 }
